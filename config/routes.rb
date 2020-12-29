f6b6a3c7f9280c56825822fd7b6fcc541e308232
takuya-kinoshita <kktak02@gmail.com>
2020-12-29 23:40:59 +0900

    fixed session id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get "/signup", to: 'users#new'
   resources :users
   root "static_pages#top"
   get '/about', to: "static_pages#about"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
